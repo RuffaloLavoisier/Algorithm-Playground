3941116df766323c636ca54bbf1787f27eb1d836
RuffaloLavoisier <jsh970307@daum.net>
2021-04-10 01:25:25 +0900

    update

--- sharepj.py
+++ sharepj.py
@@ -1,7 +1,22 @@
-for i in range(2,20):
-	for j in range(2,i+1):
-		if i % j == 0:
-			if i == j:
-				print (str(i))
-			else :
-				break
+import time # ttime lib
+
+
+def test():
+	print ("test function")
+
+	for i in range(2,20):
+		for j in range(2,i+1):
+			if i % j == 0:
+				if i == j:
+					print (str(i))
+				else :
+					break
+
+if __name__ == "__main__":
+	print ("start !")
+	print (__name__)
+	test()
+	print ("end")
+else :
+	print("import use !")
+	print(__name__)
